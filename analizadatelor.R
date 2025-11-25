17b56da269926828da98287306be4c38b1d493a1
vania13 <tiberiu.jakab@gmail.com>
2025-11-25 12:46:12 +0200

    Add files via upload

--- analizadatelor.R
+++ analizadatelor.R
@@ -8,8 +8,17 @@ X <- (read.xlsx("eurostat.xlsx", sheet = "Data",colNames = T, rowNames = T) |> c
 # round(princomp(X)$loadings,3)
 # remosummary(princomp(X), cor = T, scores = T)
 eurostat <- princomp(X)
+
+#tema 1
 colnames(eurostat$scores)[1] <- "ocupation"
 colnames(eurostat$scores)[2] <- "fertility"
 View(eurostat$scores)
+
+#tema 2
+captured <- eurostat$loadings[, 1:3]^2*100 |> round(digits=3)
+captured <- cbind(captured, rowSums(captured))
+colnames(captured)[1:4] = c("Ocupation","Fertility","Residual","Total")
+View(captured)
+
 # PCA(X, scale.unit = 'true', ncp = 15)$ind$coord
 # PCA(X, scale.unit = 'true', ncp = 15)$svd$V
